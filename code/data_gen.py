b12d6695f614b85a3b2a9311341738129ef273a4
Seq2seqAdapt <zhangyaping993@gmail.com>
2018-10-17 21:36:01 +0800

    clean up

--- code/data_gen.py
+++ code/data_gen.py
@@ -10,14 +10,15 @@ from bucketdata import BucketData
 from PIL import Image
 from six import BytesIO as IO
 
-def load_char_list(char_list_path='/home/data/OCR/IAM/words/label_dictionary.txt'):
-  charlist = []
-  with open(char_list_path) as f:
-    for l in f:
-      charlist.append(l.strip()) #.decode('utf-8'))
-  vocab_size = len(charlist)
-  print("vocab_size is:", vocab_size)
-  return charlist
+
+def load_char_list(char_list_path):
+    charlist = []
+    with open(char_list_path) as f:
+        for l in f:
+            charlist.append(l.strip())  # .decode('utf-8'))
+    vocab_size = len(charlist)
+    print("vocab_size is:", vocab_size)
+    return charlist
 
 
 class DataGen(object):
@@ -25,16 +26,10 @@ class DataGen(object):
     EOS_ID = 2
     IMAGE_HEIGHT = 32
 
-    char_list = load_char_list()
-    # char_list= list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    # char_list = load_char_list('/home/data/OCR/IAM/words/label_dictionary.txt')
+    char_list = list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     CHARMAP = ['', '', ''] + char_list
     print(CHARMAP)
-
-    # CHARMAP = ['', '', ''] + list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
-    # @staticmethod
-    # def setFullAsciiCharmap():
-    #     DataGen.CHARMAP = ['', '', ''] + [chr(i) for i in range(32, 127)]
 
     def __init__(self,
                  sess,
@@ -56,7 +51,7 @@ class DataGen(object):
         self.epochs = epochs
         self.max_width = max_width
 
-        self.bucket_specs = buckets #(encoder_size, decoder_size)
+        self.bucket_specs = buckets  # (encoder_size, decoder_size)
         self.bucket_data = BucketData()
 
         if isinstance(annotation_fn, list):
@@ -70,21 +65,21 @@ class DataGen(object):
         else:
             dataset_list = glob.glob(os.path.join(annotation_fn, '*.tfrecords'))
 
-        dataset_list.sort() #
-        if selected_num < len(dataset_list): # selected one of the specific tfrecords
+        dataset_list.sort()  #
+        if selected_num < len(dataset_list):  # selected one of the specific tfrecords
             print("selected_num < len(dataset_list)")
             selected_id = selected_num
-            selected_dataset = [os.path.join(annotation_fn, str(int(selected_id))+'.tfrecords')]
+            selected_dataset = [os.path.join(annotation_fn, str(int(selected_id)) + '.tfrecords')]
             self.data_num = 1e4
         else:
             print("selected_num > len(dataset_list)")
             if len(dataset_list) > 1 and selected_num >= 10000:
                 if int(selected_num / 10000) > len(dataset_list):
                     selected_dataset = dataset_list
-                    self.data_num = len(dataset_list)*1e4
-                    print("the total number of dataset is less than 10000*",len(dataset_list))
+                    self.data_num = len(dataset_list) * 1e4
+                    print("the total number of dataset is less than 10000*", len(dataset_list))
                 else:
-                    selected_dataset = dataset_list[:int(selected_num/10000)]
+                    selected_dataset = dataset_list[:int(selected_num / 10000)]
                     self.data_num = selected_num
                     print("selected_train_num", selected_num)
             else:
@@ -113,112 +108,27 @@ class DataGen(object):
         print("begining gen data--------")
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
             # self.sess = sess
-            i=0
+            i = 0
             while True:
                 try:
                     raw_images, raw_labels, raw_comments = sess.run([images, labels, comments])
-                    # print(raw_images)
-                    # print(raw_labels)
-                    # print(raw_comments)
                     for img, lex, comment in zip(raw_images, raw_labels, raw_comments):
-                        # print("raw_labels", lex) #b'MANAGEMENT'
-                        # print("raw_comment", comment) #b''
-                        # print("self.max_width", self.max_width) #200
                         word = self.convert_lex(lex)
-                        # print("word", word)
-                        # np.array [1 25 17 32 27 27 24  2]
-
-                        # print("bucket_size before append",bucket_size)
-
                         bucket_size = self.bucket_data.append(img, word, lex, comment)
-                        # print("bucket_size after append", bucket_size)
                         if bucket_size >= batch_size:
                             bucket = self.bucket_data.flush_out(
                                 self.bucket_specs,
                                 go_shift=1)
-                            # print("bucket_size after append", bucket_size)
                             yield bucket
                 except tf.errors.OutOfRangeError:
-                    # print("tf.errors.OutOfRangeError")
                     break
             self.clear()
-
-    def gen_0(self, batch_size):
-
-        dataset = self.dataset.batch(batch_size)
-        iterator = dataset.make_one_shot_iterator()
-
-        images, labels, comments = iterator.get_next()
-        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-            # self.sess = sess
-            i=0
-            while True:
-                try:
-                    raw_images, raw_labels, raw_comments = sess.run([images, labels, comments])
-                    for img, lex, comment in zip(raw_images, raw_labels, raw_comments):
-                        # print("raw_labels", lex) #b'MANAGEMENT'
-                        # print("raw_comment", comment) #b''
-                        # print("self.max_width", self.max_width) #200
-                        word = self.convert_lex(lex)
-                        # print("word", word)
-                        # np.array [1 25 17 32 27 27 24  2]
-
-                        # print("bucket_size before append",bucket_size)
-
-                        bucket_size = self.bucket_data.append(img, word, lex, comment)
-                        # print("bucket_size after append", bucket_size)
-                        if bucket_size >= batch_size:
-                            bucket = self.bucket_data.flush_out(
-                                self.bucket_specs,
-                                go_shift=1)
-                            # print("bucket_size after append", bucket_size)
-                            yield bucket
-
-
-                        # if self.max_width and (Image.open(IO(img)).size[0] <= self.max_width):
-                        #
-                        #
-                        #     word = self.convert_lex(lex)
-                        #     # print("word", word)
-                        #     # np.array [1 25 17 32 27 27 24  2]
-                        #
-                        #     # print("bucket_size before append",bucket_size)
-                        #
-                        #     bucket_size = self.bucket_data.append(img, word, lex, comment)
-                        #     # print("bucket_size after append", bucket_size)
-                        #     if bucket_size >= batch_size:
-                        #         bucket = self.bucket_data.flush_out(
-                        #             self.bucket_specs,
-                        #             go_shift=1)
-                        #         # print("bucket_size after append", bucket_size)
-                        #         yield bucket
-                        # else:
-                        #     i = i + 1
-                        #     print(">self.max_width",i)
-                        #     print("Image.open(IO(img)).size[0]", Image.open(IO(img)).size[0])
-                        #     print("self.bucket_specs", self.bucket_specs)
-
-
-
-                except tf.errors.OutOfRangeError:
-                    # print("tf.errors.OutOfRangeError")
-                    break
-        self.clear()
 
     def convert_lex(self, lex):
         if sys.version_info >= (3,):
             lex = lex.decode('iso-8859-1')
 
-        # print("lex.decode('iso-8859-1')", lex) # 'METOOL'
-        # print("len(lex)", len(lex)) # 6
-        #print("self.bucket_specs", self.bucket_specs) #(encoder_size, decoder_size)
-        # print("self.bucket_specs[-1][1]", self.bucket_specs[-1][1])
-
         assert len(lex) < self.bucket_specs[-1][1]
-        # print("self.GO_ID",self.GO_ID)
-        # print("self.CHARMAP", self.CHARMAP)
-        # print("self.EOS_ID", self.EOS_ID)
-
         word_label = np.array(
             [self.GO_ID] + [self.CHARMAP.index(char) for char in lex] + [self.EOS_ID],
             dtype=np.int32)
